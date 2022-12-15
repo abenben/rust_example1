eadc32967cf04f974cd9869d0c360e48a5bd7227
abenben <abenbenben@gmail.com>
2022-12-15 09:02:21 +0900

    修正

---
+++ src/main2.rs
@@ -0,0 +1,16 @@
+/*
+$ ./playground
+$ play
+$ echo "
+Hello CLI!
+This is a multi-line command.
+" > message
+$ cat message
+$ export MY_ENV=1234
+$ echo $MY_ENV
+*/
+
+fn main() {
+    let x = std::f32::consts::PI;
+    println!("Hello, world!2");
+}
